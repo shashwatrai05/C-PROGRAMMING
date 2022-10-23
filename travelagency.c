6d4785348277ee73ca904c8630db199a74167f23
ShashwatRai575 <116365030+ShashwatRai575@users.noreply.github.com>
2022-10-23 23:50:48 +0530

    Update travelagency.c

--- travelagency.c
+++ travelagency.c
@@ -1,15 +1,4 @@
 #include <stdio.h>
-/*
-You manage a travel agency and you want your n drivers to input their following details:
-1. Name
-2. Driving License No
-3. Route
-4. Kms
-Your program should be able to take n as input(or you can take n=3 for simplicity) and your drivers will start inputting their details one by one.
-
-Your program should print details of the drivers in a beautiful fashion.
-User structures.
-*/
 
 struct Driver
 {
